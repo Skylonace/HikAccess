3527dd8124a949adc134faa3b855541a3363369a
Manuel MV <manuelmartinezvaamonde@gmail.com>
2023-09-21 20:24:34 +0000

    fixed stupid mistake v3

--- HikAccess/app/app.py
+++ HikAccess/app/app.py
@@ -77,7 +77,7 @@ def edit_intercom(slot_number):
     if request.method == 'POST':
         upload_code(request.form['code'],request.form['description'], slot_number)
         return redirect(ingress_root + '/intercom')
-    return render_template('edit.html',ingress_root=ingress_root, slot_number=slot_number)
+    return render_template('edit_code.html',ingress_root=ingress_root, slot_number=slot_number)
 
 @app.teardown_appcontext
 def close_connection(exception):
